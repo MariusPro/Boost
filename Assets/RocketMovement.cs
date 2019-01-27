c263b5bf6b348fb51fcf6ee9a1abf950add27c31
Prodan Marius <mprodan27@gmail.com>
2019-01-27 14:22:07 +0200

    Adding sound on thrust

--- Assets/RocketMovement.cs
+++ Assets/RocketMovement.cs
@@ -7,10 +7,12 @@ public class RocketMovement : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,18 +22,34 @@ public class RocketMovement : MonoBehaviour
     }
 
     private void ProcessInput()
-    {
+    {/*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Stop();
+        }*/
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotate Left");
+            transform.Rotate(Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            print("Rotate Right");
+            transform.Rotate(-(Vector3.forward));
         }
     }
 }
